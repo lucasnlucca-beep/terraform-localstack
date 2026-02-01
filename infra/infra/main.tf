242dd9d9efccfb9ed592836e6e7f7f4fe3325631
root <root@Lucas-Lucca.localdomain>
2026-02-01 00:29:36 -0300

    Atualiza .gitignore e arquivos Terraform para LocalStack

--- infra/infra/main.tf
+++ infra/infra/main.tf
@@ -1,16 +1,16 @@
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  s3_force_path_style         = true
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
-}
+#provider "aws" {
+#  region                      = "us-east-1"
+#  access_key                  = "test"
+#  secret_key                  = "test"
+#  skip_credentials_validation = true
+#  skip_metadata_api_check     = true
+#  s3_force_path_style         = true
+#  endpoints {
+#    s3 = "http://localhost:4566"
+#  }
+#}
 
-resource "aws_s3_bucket" "meu_bucket" {
-  bucket = "meu-bucket-localstack"
-  acl    = "private"
-}
+#resource "aws_s3_bucket" "meu_bucket" {
+#  bucket = "meu-bucket-localstack"
+#  acl    = "private"
+#}
