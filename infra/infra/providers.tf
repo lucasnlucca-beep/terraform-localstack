1b29b3700e2cdc124a797dd3b1299a50a80871d6
root <root@Lucas-Lucca.localdomain>
2026-02-01 00:33:00 -0300

    Atualiza .gitignore e arquivos Terraform para LocalStack

--- infra/infra/providers.tf
+++ infra/infra/providers.tf
@@ -8,5 +8,8 @@ provider "aws" {
 
   endpoints {
     s3 = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
   }
 }
