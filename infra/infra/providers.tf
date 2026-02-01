7e98f000c8fece966a9720196462050a241b8d91
root <root@Lucas-Lucca.localdomain>
2026-02-01 01:14:02 -0300

    Atualiza .gitignore e arquivos Terraform para LocalStack

--- infra/infra/providers.tf
+++ infra/infra/providers.tf
@@ -3,6 +3,7 @@ provider "aws" {
   access_key                  = "nofewinf38932-2"
   secret_key                  = "idonw9wqn92dwqeiode"
   skip_credentials_validation = true
+  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
 
   endpoints {
